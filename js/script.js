4624b84211a7f1885dead478b53dee341e38e415
awadh <awadhhassan566@gmail.com>
2020-12-21 13:12:42 +0300

    addition of clickable function

---
+++ js/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+  $(".design-shown").click(function(){
+    $(".design-shown").toggle();
+    $(".design-hidden").toggle();
+
+   });
+
+    $(".design-shown").click(function(){
+        $(".dev-shown").toggle();
+        $(".dev-hidden").toggle();
+
+        
+});
+    $(".product-shown").click(function(){
+        $(".product-shown").toggle();
+        $(".product-hidden").toggle();
+
+    });
+
+
+});
