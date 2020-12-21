ab3d61f82e4818662e6d844a0c83be6f42366acd
awadh <awadhhassan566@gmail.com>
2020-12-22 00:21:15 +0300

    addition of of submit alert function

--- js/script.js
+++ js/script.js
@@ -1,21 +1,32 @@
-$(document).ready(function(){
-  $(".design-shown").click(function(){
-    $(".design-shown").toggle();
-    $(".design-hidden").toggle();
+$(document).ready(function () {
+  $(".design").click(function () {
+    $(".design").toggle();
+    $(".card-body").toggle();
+  });
 
-   });
-
-    $(".design-shown").click(function(){
-        $(".dev-shown").toggle();
-        $(".dev-hidden").toggle();
-
-        
+  $(".development").click(function () {
+    $(".development").toggle();
+    $(".card-boy").toggle();
+  });
+  $(".product").click(function () {
+    $(".product").toggle();
+    $(".card-bdy").toggle();
+  });
+$(".submit").click(function(){
+  alert("we have recieved your message thank you for reaching out to us");
 });
-    $(".product-shown").click(function(){
-        $(".product-shown").toggle();
-        $(".product-hidden").toggle();
+  
 
-    });
+  //    $(".blanks form").submit(function(event) {
+  //     var nameInput = $("input.nameInput").val();
+  //     var emailInput = $("input.emailInput").val();
+  //     var commentInput=$("input.commentInput").val();
+  //     $(".name").append(nameInput);
+  //     $(".email").append(emailInput);
+  //     $("comment").append(commentInput);
+  //     $("#story").show();
 
-
+  //     event.preventDefault();
+  //     alert("thank you very much for your comment")
+  //    });
 });
