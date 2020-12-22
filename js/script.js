052dc8c01171d5aba601f02e19cbb3368b6476ff
awadh <awadhhassan566@gmail.com>
2020-12-22 10:24:46 +0300

    deletion of javascript code

--- js/script.js
+++ js/script.js
@@ -17,16 +17,4 @@ $(".submit").click(function(){
 });
   
 
-  //    $(".blanks form").submit(function(event) {
-  //     var nameInput = $("input.nameInput").val();
-  //     var emailInput = $("input.emailInput").val();
-  //     var commentInput=$("input.commentInput").val();
-  //     $(".name").append(nameInput);
-  //     $(".email").append(emailInput);
-  //     $("comment").append(commentInput);
-  //     $("#story").show();
-
-  //     event.preventDefault();
-  //     alert("thank you very much for your comment")
-  //    });
 });
