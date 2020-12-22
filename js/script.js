40a9a5786fba5dd18dcc3926d99c014b9e0d2205
awadh <awadhhassan566@gmail.com>
2020-12-22 13:16:33 +0300

    addition of js code

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
   $(".design").click(function () {
     $(".design").toggle();
-    $(".card-body").toggle();
+    $(".card-body44").toggle();
   });
 
   $(".development").click(function () {
@@ -12,9 +12,12 @@ $(document).ready(function () {
     $(".product").toggle();
     $(".card-bdy").toggle();
   });
-$(".submit").click(function(){
-  alert("we have recieved your message thank you for reaching out to us");
-});
+
+// $(".submit").click(function(){
+//   alert("we have recieved your message thank you for reaching out to us");
+// });
   
+let imgUrl = 'images/c_image.jpg'
+  $(".comment-section").css("background-image",'url('+imgUrl+')');
 
 });
